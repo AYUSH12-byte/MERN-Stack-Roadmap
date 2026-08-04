41f98d9c495472c88f634f69a57bcb2607c77242
Ayush <achy93858@gmail.com>
2026-08-04 08:14:59 +0545

    updates usercontoller

--- Internship/simple-crud-main/controller/userController.js
+++ Internship/simple-crud-main/controller/userController.js
@@ -1,28 +1,26 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 // CREATE USER
 exports.createUser = async (req, res) => {
   try {
-  const{ name, email, age} = req.body;
+    const { name, email, age } = req.body;
 
-  const imagePath=req.file? req.file.path:null;
+    const imagePath = req.file ? req.file.path : null;
 
-  //  const imagePath = req.files?.[1]?.path || null; for multiple image uploads
+    //  const imagePath = req.files?.[1]?.path || null; for multiple image uploads
 
     // const imagePath= req.files? req.files.map(f=>f.path): null;
 
     // const frontpath = req.files? req.files['front'][0].path :null;
     //  const backpath = req.files? req.files['back'][0].path :null;
-     
 
-
-   console.log(imagePath ,"this is the image path");
+    console.log(imagePath, "this is the image path");
 
     const user = await User.create({ name, email, age, image: imagePath });
 
     res.status(201).json({
       success: true,
-      message: 'User created successfully',
+      message: "User created successfully",
       data: user,
     });
   } catch (error) {
@@ -52,7 +50,7 @@ exports.getUserById = async (req, res) => {
     if (!user) {
       return res.status(404).json({
         success: false,
-        message: 'User not found',
+        message: "User not found",
       });
     }
 
@@ -73,13 +71,13 @@ exports.updateUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({
         success: false,
-        message: 'User not found',
+        message: "User not found",
       });
     }
 
     res.status(200).json({
       success: true,
-      message: 'User updated successfully',
+      message: "User updated successfully",
       data: user,
     });
   } catch (error) {
@@ -95,13 +93,13 @@ exports.deleteUser = async (req, res) => {
     if (!user) {
       return res.status(404).json({
         success: false,
-        message: 'User not found',
+        message: "User not found",
       });
     }
 
     res.status(200).json({
       success: true,
-      message: 'User deleted successfully',
+      message: "User deleted successfully",
     });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
